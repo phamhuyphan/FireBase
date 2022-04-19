6fcfa859d0037fe6e92719b6c726ed7cdb6c6b16
phamhuyphan <huyphanpham29092001@gmail.com>
2022-04-19 07:22:17 +0700

    FireBase

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button edibutton = findViewById(R.id.signin);
+
+        View.OnClickListener listenerEditbutton = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+
+                intent.setClass(view.getContext(),MainActivity2.class);
+
+                startActivity(intent);
+            }
+        };
+        edibutton.setOnClickListener(listenerEditbutton);
+    }
+
+}
